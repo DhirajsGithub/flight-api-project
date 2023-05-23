35b02f1e764123523028b849fb06966443705d35
Dhiraj Borse <borsedhiraj123@gmail.com>
2023-05-23 23:39:19 +0530

    small changes

--- src/App.js
+++ src/App.js
@@ -1,27 +1,243 @@
-import * as React from 'react';
-import Map, {NavigationControl} from 'react-map-gl';
-import Navbar from './components/navbar.js';
-import maplibregl from 'maplibre-gl';
-import 'maplibre-gl/dist/maplibre-gl.css';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import Map, { NavigationControl, Marker } from "react-map-gl";
+import Navbar from "./components/navbar.js";
+import maplibregl from "maplibre-gl";
+import "maplibre-gl/dist/maplibre-gl.css";
+import "./App.css";
+import planeImg from "./assets/Images/Prognosticz_Plane.png";
 
 function App() {
+  // NOTE: we are only fetching for airline BAW and Aircraft type A319
+  const headers = {
+    Authorization: "Bearer a86afcfe8d09c9f9317ce87c720af3c955bc946f",
+    "Content-Type": "application/json",
+  };
+  const bodyForAircrafts = {
+    pageSize: 10,
+    page: 1,
+    registrations: [
+      "G-YMML",
+      "G-ZZZA",
+      "G-VIIA",
+      "G-VIIB",
+      "G-VIIC",
+      "G-VIID",
+      "G-VIIE",
+      "G-VIIF",
+      "G-VIIG",
+      "G-VIIH",
+      "G-RAES",
+      "G-VIIJ",
+      "G-VIIK",
+      "G-VIIL",
+      "G-VIIM",
+      "G-VIIN",
+      "G-VIIO",
+      "G-VIIP",
+      "G-VIIR",
+      "G-VIIS",
+    ],
+    flightIds: [""],
+    airlines: ["BAW"],
+    aircraftTypes: ["B772"],
+    incLastKnownPos: true,
+    minLatitude: 0.431234,
+    maxLatitude: 51.509776,
+    minLongitude: -2.962865,
+    maxLongitude: -0.372603,
+    fromAirports: [""],
+    toAirports: [""],
+    toOrFromAirports: [""],
+  };
+  const [airlinesStatistics, setAirlinesStatistics] = useState([]);
+  const [airlinesName, setAilinesName] = useState([]);
+  const [aircraftTypesStatics, setAircraftTypesStatics] = useState([]);
+  const [aircraftTypesName, setAircraftTypesName] = useState([]);
+  const [registrationNames, setRegistrationNames] = useState([]);
+  const [aircraftsDetails, setAircraftDetails] = useState([]);
+  const fetchAirlines = async () => {
+    let res = await fetch("https://api.radarbox.com/v2/statistics/airlines", {
+      headers,
+    });
+    res = await res.json();
+    let temp = [];
+    // fetching available airline for latest "day": "2023-05-21", on may 23
+    for (let item of res.airlineStatistics[0].statistics) {
+      temp.push(item.icaoCode);
+    }
+    setAilinesName(temp);
+    setAirlinesStatistics(res.airlineStatistics[0].statistics);
+  };
+  const fetchAircraftTypes = async () => {
+    let res = await fetch(
+      "https://api.radarbox.com/v2/statistics/airline/BAW",
+      {
+        headers,
+      }
+    );
+    res = await res.json();
+    let temp = [];
+    // this is completly based on airline type
+    for (let item of res.airlineStatistics[0].aircraftUtilization) {
+      temp.push(item.aircraftType);
+    }
+    setAircraftTypesName(temp);
+  };
+  const fetchRegistrationNames = async () => {
+    let res = await fetch(
+      "https://api.radarbox.com/v2/aircraft/search?aircraftType=B772&airline=BAW",
+      {
+        headers,
+      }
+    );
+    res = await res.json();
+    let temp = [];
+    for (let item of res.aircraft) {
+      temp.push(item.registration);
+    }
+    setRegistrationNames(temp);
+  };
+  const fetchAircraftsDetails = async () => {
+    let res = await fetch("https://api.radarbox.com/v2/flights/live", {
+      method: "POST",
+      headers,
+      body: JSON.stringify(bodyForAircrafts),
+    });
+    res = await res.json();
+    let temp = [];
+    for (let item of res.flights) {
+      temp.push({ latitude: item.latitude, longitude: item.longitude });
+    }
+    setAircraftDetails(temp);
+  };
+  console.log(aircraftTypesName);
+  console.log(registrationNames);
+  console.log(aircraftsDetails);
+
+  let iteration = parseInt(registrationNames.length / 20);
+  console.log(iteration, +" " + registrationNames.length);
+
+  useEffect(() => {
+    try {
+      fetchAircraftTypes();
+      fetchAirlines();
+      fetchRegistrationNames();
+      fetchAircraftsDetails();
+    } catch (error) {}
+  }, []);
   return (
     <div className="App">
-      <Navbar/>
-      <Map mapLib={maplibregl} 
+      {/* <Navbar /> */}
+      <Map
+        mapLib={maplibregl}
         initialViewState={{
           longitude: 16.62662018,
           latitude: 49.2125578,
-          zoom: 14
+          zoom: 1,
         }}
-        style={{width: "100%", height: " calc(100vh - 77px)"}}
-        mapStyle="https://api.maptiler.com/maps/streets/style.json?key=YOUR_MAPTILER_API_KEY_HERE"
+        style={{ width: "100%", height: "100vh" }}
+        mapStyle="https://api.maptiler.com/maps/streets/style.json?key=QPnlVAUQb7gRPLYjPXjW	"
       >
         <NavigationControl position="top-left" />
+        {aircraftsDetails?.map((item, index) => {
+          if (item.latitude !== undefined && item.longitude !== undefined) {
+            return (
+              <Marker
+                anchor="center"
+                latitude={item.latitude}
+                longitude={item.longitude}
+              >
+                <img style={{ width: "50px", height: "50px" }} src={planeImg} />
+              </Marker>
+            );
+          }
+        })}
       </Map>
     </div>
   );
 }
 
 export default App;
+
+// [
+//   "G-EUPD",
+//   "G-EUPE",
+//   "G-EUPG",
+//   "G-EUPJ",
+//   "G-EUPK",
+//   "G-EUPL",
+//   "G-EUPM",
+//   "G-EUPN",
+//   "G-EUPO",
+//   "G-EUPP",
+//   "G-EUPR",
+//   "G-EUPS",
+//   "G-EUPT",
+//   "G-EUPU",
+//   "G-EUPW",
+//   "G-EUPY",
+//   "G-EUPZ",
+//   "G-EUOA",
+//   "G-EUOC",
+//   "G-EUOD",
+//   "G-EUOE",
+//   "G-EUOG",
+//   "G-EUOF",
+//   "G-DBCA",
+//   "G-DBCC",
+//   "G-DBCB",
+//   "G-DBCD",
+//   "G-DBCE",
+//   "G-DBCF",
+//   "G-DBCG",
+//   "G-DBCH",
+//   "G-DBCJ",
+//   "G-DBCK",
+// ];
+
+// [
+//   "G-ZZZA",
+//   "G-VIIA",
+//   "G-VIIB",
+//   "G-VIIC",
+//   "G-VIID",
+//   "G-VIIE",
+//   "G-VIIF",
+//   "G-VIIG",
+//   "G-VIIH",
+//   "G-RAES",
+//   "G-VIIJ",
+//   "G-VIIK",
+//   "G-VIIL",
+//   "G-VIIM",
+//   "G-VIIN",
+//   "G-VIIO",
+//   "G-VIIP",
+//   "G-VIIR",
+//   "G-VIIS",
+//   "G-VIIT",
+//   "G-VIIU",
+//   "G-VIIV",
+//   "G-VIIW",
+//   "G-VIIX",
+//   "G-VIIY",
+//   "G-YMMA",
+//   "G-YMMB",
+//   "G-YMMC",
+//   "G-YMMD",
+//   "G-YMME",
+//   "G-YMMF",
+//   "G-YMMG",
+//   "G-YMMH",
+//   "G-YMMI",
+//   "G-YMMJ",
+//   "G-YMMK",
+//   "G-YMML",
+//   "G-YMMN",
+//   "G-YMMO",
+//   "G-YMMP",
+//   "G-YMMR",
+//   "G-YMMS",
+//   "G-YMMT",
+//   "G-YMMU"
+// ]
--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
@@ -15,3 +15,62 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+let p = {
+  cost: 1,
+  success: true,
+  comment: "Successfully fetched details for 1 flights",
+  flights: [
+    {
+      flightNumberIata: "BA142",
+      flightNumberIcao: "BAW142",
+      aircraftRegistration: "G-YMML",
+      aircraftType: "B772",
+      aircraftClasses: ["AIRLINER"],
+      aircraftTypeDescription: "Boeing 777-236ER",
+      airlineIata: "BA",
+      airlineIcao: "BAW",
+      airlineName: "British Airways",
+      depAirportIcao: "VIDP",
+      depAirportIata: "DEL",
+      depAirportName: "Indira Gandhi International Airport",
+      depAirportCity: "New Delhi",
+      depAirportState: "Delhi",
+      depAirportCountry: "India",
+      depAirportCountryIso2: "IN",
+      depAirportCountryIso3: "IND",
+      depAirportLatitude: 28.5665,
+      depAirportLongitude: 77.1031,
+      depAirportTz: "IST",
+      depAirportTzDiffUTC: "5.5",
+      scheduledDeparture: "2023-05-23T20:05:00.000+00:00",
+      estimatedDeparture: "2023-05-23T20:33:00.000+00:00",
+      arrAirportIcao: "EGLL",
+      arrAirportIata: "LHR",
+      arrAirportName: "London Heathrow Airport",
+      arrAirportCity: "London Heathrow",
+      arrAirportState: "England",
+      arrAirportCountry: "United Kingdom",
+      arrAirportCountryIso2: "GB",
+      arrAirportCountryIso3: "GBR",
+      arrAirportLatitude: 51.4706,
+      arrAirportLongitude: -0.461941,
+      arrAirportTz: "BST",
+      arrAirportTzDiffUTC: "1.0",
+      scheduledArrival: "2023-05-24T05:50:00.000+00:00",
+      estimatedArrival: "2023-05-24T06:18:00.000+00:00",
+      departureStatus: "DY",
+      departureDelayReason: "6",
+      departureDelayDetail: "ETD",
+      departureTerminal: "3",
+      arrivalStatus: "DY",
+      arrivalDelayReason: "6",
+      arrivalDelayDetail: "ETA",
+      arrivalTerminal: "5",
+      source: "SCHE",
+      created: "2023-05-22T01:30:49.000+00:00",
+      updated: "2023-05-23T11:34:28.536+00:00",
+      status: "SCHEDULED",
+    },
+  ],
+};
